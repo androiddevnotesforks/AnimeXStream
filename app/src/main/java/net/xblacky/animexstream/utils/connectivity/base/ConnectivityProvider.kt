cbe7af9dad2d919cb05384b948063bf63b667d3f
Snehil <snehil101@gmail.com>
2020-07-02 23:24:54 +0530

    For checking internet connectivity

---
+++ app/src/main/java/net/xblacky/animexstream/utils/connectivity/base/ConnectivityProvider.kt
@@ -0,0 +1,52 @@
+package net.xblacky.animexstream.utils.connectivity.base
+
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import android.net.NetworkInfo
+import android.os.Build
+import androidx.annotation.RequiresApi
+import net.xblacky.animexstream.utils.connectivity.ConnectivityProviderImpl
+import net.xblacky.animexstream.utils.connectivity.ConnectivityProviderLegacyImpl
+
+interface ConnectivityProvider {
+    interface ConnectivityStateListener {
+        fun onStateChange(state: NetworkState)
+    }
+
+    fun addListener(listener: ConnectivityStateListener)
+    fun removeListener(listener: ConnectivityStateListener)
+
+    fun getNetworkState(): NetworkState
+
+    @Suppress("MemberVisibilityCanBePrivate", "CanBeParameter")
+    sealed class NetworkState {
+        object NotConnectedState : NetworkState()
+
+        sealed class ConnectedState(val hasInternet: Boolean) : NetworkState() {
+
+            @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+            data class Connected(val capabilities: NetworkCapabilities) : ConnectedState(
+                capabilities.hasCapability(NET_CAPABILITY_INTERNET)
+            )
+
+            @Suppress("DEPRECATION")
+            data class ConnectedLegacy(val networkInfo: NetworkInfo) : ConnectedState(
+                networkInfo.isConnectedOrConnecting
+            )
+        }
+    }
+
+    companion object {
+        fun createProvider(context: Context): ConnectivityProvider {
+            val cm = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                ConnectivityProviderImpl(cm)
+            } else {
+                ConnectivityProviderLegacyImpl(context, cm)
+            }
+        }
+    }
+}
